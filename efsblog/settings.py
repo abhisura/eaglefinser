e7e3c9d3baf86c762e49fa2d751f81f8327cd640
abhisura <ssura@unomaha.edu>
2017-07-26 00:07:55 -0500

    3rd part assignment

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio'
+    'portfolio',
+    'mathfilters',
+    'rest_framework',
     ]
 
 MIDDLEWARE = [
